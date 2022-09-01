bf0dddada3e6c1aaf393b7a6a3f3993ea8299cb2
DevelopJKong <ybaram40@gmail.com>
2022-09-02 07:57:08 +0900

    #2.8 Tab Bar Icons 완료
    1. screenOptions 학습하기

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import Tabs from "./navigation/Tabs";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, DarkTheme, DefaultTheme } from "@react-navigation/native";
 import React from "react";
+import { useColorScheme } from "react-native";
 
 export default function App() {
+  const isDark = useColorScheme();
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
       <Tabs />
     </NavigationContainer>
   );
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,30 +1,69 @@
-import { View, Text } from "react-native";
+import { useColorScheme, StatusBar } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
 import Movie from "../screens/Movie";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
+import { Ionicons } from "@expo/vector-icons";
+import { BLACK_COLOR, DARK_GREY, LIGHT_GREY, YELLOW_COLOR } from "../colors";
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
+  const isDark = useColorScheme();
   return (
-    <Tab.Navigator screenOptions={{ tabBarStyle: { backgroundColor: "tomato" }, tabBarActiveTintColor: "red" }}>
-      <Tab.Screen
-        name="Movies"
-        component={Movie}
-        options={{
-          headerTitleStyle: { color: "tomato" },
-          headerRight: () => (
-            <View>
-              <Text>Hello</Text>
-            </View>
-          ),
+    <>
+      <StatusBar backgroundColor="#1e272e" style={{ color: "white" }} />
+      <Tab.Navigator
+        screenOptions={{
+          tabBarStyle: { backgroundColor: isDark ? BLACK_COLOR : "white" },
+          tabBarActiveTintColor: isDark ? YELLOW_COLOR : BLACK_COLOR,
+          tabBarInactiveTintColor: isDark ? DARK_GREY : LIGHT_GREY,
+          headerStyle: {
+            backgroundColor: isDark ? BLACK_COLOR : "white",
+          },
+          headerTitleStyle: {
+            color: isDark ? "white" : BLACK_COLOR,
+          },
+          tabBarLabelStyle: {
+            marginTop:-5,
+            fontSize: 12,
+            fontWeight: 500
+          }
         }}
-      />
-      <Tab.Screen name="Tv" component={Tv} options={{ tabBarBadge: 5 }} />
-      <Tab.Screen name="Search" component={Search} />
-    </Tab.Navigator>
+      >
+        <Tab.Screen
+          name="Movie"
+          component={Movie}
+          options={{
+            tabBarIcon: ({ focused, color, size }) => {
+              console.log(focused, color, size);
+              return <Ionicons name="film-outline" color={color} size={size} />;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="TV"
+          component={Tv}
+          options={{
+            tabBarIcon: ({ focused, color, size }) => {
+              console.log(focused, color, size);
+              return <Ionicons name="tv-outline" color={color} size={size} />;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Search"
+          component={Search}
+          options={{
+            tabBarIcon: ({ focused, color, size }) => {
+              console.log(focused, color, size);
+              return <Ionicons name="search-outline" color={color} size={size} />;
+            },
+          }}
+        />
+      </Tab.Navigator>
+    </>
   );
 };
 
